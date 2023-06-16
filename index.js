38e57257dd5c29f796435243eb336fc2b7f74876
Ian Raburu <williamraburu@gmail.com>
2023-06-16 02:36:42 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+
+function upperCaseCustomerName() {
+    customerName = "BOB";
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    overwrite = 'maybe bob';
+    bestCustomer = overwrite;
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = "NObody";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "SOmeone";
+    return leastFavoriteCustomer;
+}
+
